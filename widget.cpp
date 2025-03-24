68a25f83f8ad77cf27237d6e205411ef1be2f295
liziyi <2152291487@qq.com>
2025-03-24 18:28:27 +0800

    feat:设置电压阈值时与当前电量比较，实时更新状态灯，不在依赖获取物理参数命令 fix:调整状态灯区域的比例，适应不同大小的屏幕

--- widget.cpp
+++ widget.cpp
@@ -1244,8 +1244,18 @@ void Widget::on_setVolThresholdBtn_clicked()
 {
     // 获取 QLineEdit 中的数据
     QString data = ui->setVolThresholdBtnLineEdit->text();
-
+    float currentData=phyPara.batPercent;
+    qDebug()<<"data:"<<data<<"\ncurrentData:"<<currentData;
     saveToJson("VolThreshold", data);
+    //WARNING：未判断toFloat是否成功
+    if(currentData<=(data.toFloat()))//当前电量小于阈值
+    {
+        ui->batStateLitLabel->setPixmap(redLit.scaled(60,60));//红灯
+    }
+    else
+    {
+        ui->batStateLitLabel->setPixmap(greenLit.scaled(60,60));//绿灯
+    }
 }
 
 void Widget::on_setNormalMessFreqBtn_clicked()
